775f09651424398c3585b7a53c65999b5509d0b0
Pixelistik <code@pixelistik.de>
2018-02-03 21:59:07 +0100

    Document how rules work to allow extension

--- src/Rules.js
+++ src/Rules.js
@@ -1,17 +1,42 @@
 const Rules = {
+  /*
+   * FORBIDDEN_WORDS:
+   * A list of words that should be avoided in your texts. Occurences will be
+   * reported as forbidden, without giving an alternative.
+   */
   FORBIDDEN_WORDS: [
     'React',
     'Java',
     'Internet Explorer',
   ],
+
+  /*
+   * COMMON_MISTAKES:
+   * Pairs of wrong + correct. Occurences of the wrong versions will be
+   * reported and recommended to be changed into the correct version.
+   */
   COMMON_MISTAKES: [
     { wrong: 'LKW', correct: 'Lkw' },
     { wrong: 'FireFox', correct: 'Firefox' },
   ],
+
+  /*
+   * COMMON_WORDS:
+   * A list of words that are important to be spelled correctly. The text is
+   * checked for slightly different occurences of these words, e.g. typos.
+   */
   COMMON_WORDS: [
     'Software',
     'Quality Assurance',
   ],
+
+  /*
+   * REGEX:
+   * A list of regular expressions. Each match of these is reported, giving the
+   * description. The description should include an explanation why this occurence
+   * is wrong. Optionally, you can include a correct version, using matches
+   * marked with () in the regex.
+   */
   REGEX: [
     {
       regex: /"(.+)"/g,
